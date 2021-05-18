d719420f2bf6979336483660e29315ccbf2039be
Matt Kuhns <kuhns_m@yahoo.com>
2021-05-18 07:59:55 -0700

    Modify deductions to look nicer

--- client/src/Pages/Employees/Deductions.tsx
+++ client/src/Pages/Employees/Deductions.tsx
@@ -1,60 +1,52 @@
 import Form from "@rjsf/bootstrap-4";
 import { JSONSchema7 } from "json-schema";
 import { useHistory } from "react-router-dom";
+import {
+  ObjectFieldTemplate,
+  CustomFieldTemplate,
+  ArrayFieldTemplate,
+} from "../../Components/RJSFTemplates";
+
 export default function Deduction(props: any) {
   const history = useHistory();
+
   const schema: JSONSchema7 = {
     type: "object",
-
     properties: {
       deductions: {
         title: "Deductions",
         type: "array",
         items: {
           type: "object",
-          required: ["name", "amount"],
+          required: ["name"],
           properties: {
             name: {
-              type: "string",
               title: "Name",
+              type: "string",
             },
             pretax: {
               type: "boolean",
               title: "Pretax deduction",
             },
-          },
-          oneOf: [
-            {
-              title: "Flat amount",
-              properties: {
-                amount: {
-                  type: "number",
-                  title: "Amount",
-                },
-              },
-              required: ["amount"],
+            flatAmount: {
+              type: "number",
+              title: "Flat Amount",
             },
-            {
+            percentage: {
+              type: "number",
               title: "Percentage",
-              properties: {
-                percentage: {
-                  type: "number",
-                  title: "Percentage",
-                },
-              },
-              required: ["percentage"],
+              minimum: 1,
+              maximum: 100,
             },
-          ],
+          },
         },
       },
     },
   };
 
   const uiSchema = {
-    deductions: {
-      "ui:options": {
-        orderable: false,
-      },
+    "ui:options": {
+      orderable: false,
     },
   };
 
@@ -68,6 +60,13 @@ export default function Deduction(props: any) {
     history.push("/employee/federal-taxes");
   };
 
+  function validate(formData: any, errors: any) {
+    if (formData.flatAmount === "" && formData.percentage === "") {
+      errors.flatAmount.addError("Flat amount or percentage must be populated");
+    }
+    return errors;
+  }
+
   return (
     <Form
       className="p-3"
@@ -76,6 +75,10 @@ export default function Deduction(props: any) {
       uiSchema={uiSchema}
       formData={formData}
       onSubmit={onSubmit}
+      validate={validate}
+      ObjectFieldTemplate={ObjectFieldTemplate}
+      ArrayFieldTemplate={ArrayFieldTemplate}
+      FieldTemplate={CustomFieldTemplate}
     >
       <div>
         <button className="btn btn-primary float-left" onClick={() => onBack()}>
